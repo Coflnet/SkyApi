66831c344463818a2299d90602d6916baeb92b02
Äkwav <xekwavx@gmail.com>
2022-01-22 17:29:24 +0100

    remove command dependency from player endpoints

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using hypixel;
 using Microsoft.AspNetCore.Mvc;
 using Coflnet.Sky.Api.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coflnet.Hypixel.Controller
 {
@@ -12,8 +14,16 @@ namespace Coflnet.Hypixel.Controller
     public class PlayerController : ControllerBase
     {
         const int pageSize = 10;
+        HypixelContext context;
+
+        public PlayerController(HypixelContext context)
+        {
+            this.context = context;
+        }
+
+
         /// <summary>
-        /// The last 10 auctions a player bid on
+        /// The last 10 bids (with auction) a player did
         /// </summary>
         /// <param name="playerUuid">The uuid of the player</param>
         /// <param name="page">Page of auctions (another 10)</param>
@@ -23,14 +33,54 @@ namespace Coflnet.Hypixel.Controller
         public async Task<List<BidResult>> GetPlayerBids(string playerUuid, int page = 0)
         {
             AssertUuid(playerUuid);
-            var result = await CoreServer.ExecuteCommandWithCache<PaginatedRequest, List<BidResult>>(
-                "playerBids", new PaginatedRequest()
-                {
-                    Amount = pageSize,
-                    Offset = pageSize * page,
-                    Uuid = playerUuid
-                });
-            return result;
+            var offset = pageSize * page;
+            var playerBids = await context.Bids.Where(b => b.BidderId == context.Players.Where(p => p.UuId == playerUuid).Select(p => p.Id).FirstOrDefault())
+                    // filtering
+                    .OrderByDescending(auction => auction.Id)
+                        .Skip(offset)
+                        .Take(pageSize)
+                    //.Include (p => p.Auction)
+                    .Select(b => new
+                    {
+                        b.Auction.Uuid,
+                        b.Auction.ItemName,
+                        b.Auction.Tag,
+                        b.Auction.HighestBidAmount,
+                        b.Auction.End,
+                        b.Amount,
+                        b.Auction.StartingBid,
+                        b.Auction.Bin
+
+                    }).GroupBy(b => b.Uuid)
+                    .Select(bid => new
+                    {
+                        bid.Key,
+                        Amount = bid.Max(b => b.Amount),
+                        HighestBid = bid.Max(b => b.HighestBidAmount),
+                        ItemName = bid.Max(b => b.ItemName),
+                        Tag = bid.Max(b => b.Tag),
+                        HighestOwnBid = bid.Max(b => b.Amount),
+                        End = bid.Max(b => b.End),
+                        StartBid = bid.Max(b => b.StartingBid),
+                        Bin = bid.Max(b => b.Bin)
+                    })
+                    .ToListAsync();
+
+            var aggregatedBids = playerBids
+                        .Select(b => new BidResult()
+                        {
+                            HighestBid = b.HighestBid,
+                            AuctionId = b.Key,
+                            End = b.End,
+                            HighestOwnBid = b.HighestOwnBid,
+                            ItemName = b.ItemName,
+                            Tag = b.Tag,
+                            StartingBid = b.StartBid,
+                            Bin = b.Bin
+                        })
+                        .OrderByDescending(b => b.End)
+                        .ToList();
+            return aggregatedBids;
         }
 
         /// <summary>
@@ -44,14 +94,17 @@ namespace Coflnet.Hypixel.Controller
         public async Task<List<AuctionResult>> GetPlayerAuctions(string playerUuid, int page = 0)
         {
             AssertUuid(playerUuid);
-            var result = await CoreServer.ExecuteCommandWithCache<PaginatedRequest, List<AuctionResult>>(
-                "playerAuctions", new PaginatedRequest()
-                {
-                    Amount = pageSize,
-                    Offset = pageSize * page,
-                    Uuid = playerUuid
-                });
-            return result;
+            var offset = pageSize * page;
+            var batch = await context.Auctions
+                        .Where(a => a.SellerId == context.Players.Where(p => p.UuId == playerUuid).Select(p => p.Id).FirstOrDefault())
+                        .OrderByDescending(a => a.Id)
+                        .Skip(offset)
+                        .Take(pageSize)
+                        .ToListAsync();
+
+            return batch.OrderByDescending(a => a.End)
+                    .Select(a => new AuctionResult(a))
+                    .ToList();
         }
 
         /// <summary>
