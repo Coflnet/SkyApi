b938047519676cc81d85ba7ad0f34e4e66ea1da6
Äkwav <xekwavx@gmail.com>
2022-02-15 18:57:19 +0100

    add bazaar item list endpoint

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coflnet.Sky.Api.Services;
+using Coflnet.Sky.Commands;
+using Coflnet.Sky.Commands.MC;
+using hypixel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Coflnet.Hypixel.Controller
+{
+    /// <summary>
+    /// Endpoints about items
+    /// </summary>
+    [ApiController]
+    [Route("api/items")]
+    [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any, NoStore = false)]
+    public class ItemController : ControllerBase
+    {
+        private IConfiguration config;
+        private HypixelContext db;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="FlipController"/>
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="context"></param>
+        public ItemController(IConfiguration config, HypixelContext context)
+        {
+            this.config = config;
+            this.db = context;
+        }
+
+        /// <summary>
+        /// A list of item tags (hypixel ids) that are tradeable on bazaar
+        /// This gets updated once every hour
+        /// </summary>
+        /// <returns></returns>
+        [Route("bazaar/tags")]
+        [HttpGet]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public async Task<IEnumerable<string>> GetFlipTime()
+        {
+            return await db.Items.Where(i => i.IsBazaar).Select(i => i.Tag).ToListAsync();
+        }
+    }
+}
+
