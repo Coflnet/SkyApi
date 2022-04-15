09df6cc7b0cc5e8d78f68ffb21512e2767ab2bc6
Äkwav <xekwavx@gmail.com>
2022-04-15 21:29:00 +0200

    remove warning by documenting topupargs

--- Models/TopUpArguments.cs
+++ Models/TopUpArguments.cs
@@ -1,7 +1,14 @@
 namespace Coflnet.Sky.Api.Models
 {
+    /// <summary>
+    /// Additional arguments for a topup
+    /// </summary>
     public class TopUpArguments
     {
+        /// <summary>
+        /// A specific amount of coins to topup
+        /// </summary>
+        /// <value></value>
         public int CoinAmount { get; set; }
     }
 }
