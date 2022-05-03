93c83dd19eba6e8726d206091377dbd4a4e5f431
Äkwav <xekwavx@gmail.com>
2022-05-03 19:03:26 +0200

    dump description details

--- Services/ModDescriptionService.cs
+++ Services/ModDescriptionService.cs
@@ -6,6 +6,7 @@ using Coflnet.Sky.Core;
 using Coflnet.Sky.Crafts.Client.Api;
 using Coflnet.Sky.Sniper.Client.Api;
 using Newtonsoft.Json;
+using OpenTracing;
 
 namespace Coflnet.Sky.Api.Services
 {
@@ -13,11 +14,13 @@ namespace Coflnet.Sky.Api.Services
     {
         private ICraftsApi craftsApi;
         private ISniperApi sniperApi;
+        private ITracer tracer;
 
-        public ModDescriptionService(ICraftsApi craftsApi, ISniperApi sniperApi)
+        public ModDescriptionService(ICraftsApi craftsApi, ISniperApi sniperApi, ITracer tracer)
         {
             this.craftsApi = craftsApi;
             this.sniperApi = sniperApi;
+            this.tracer = tracer;
         }
 
         public async Task<IEnumerable<string[]>> GetDescriptions(InventoryData inventory)
@@ -56,6 +59,7 @@ namespace Coflnet.Sky.Api.Services
             }).ToList());
             var allCrafts = await allCraftsTask;
             Console.WriteLine(JsonConvert.SerializeObject(allCrafts));
+            var span = tracer.ActiveSpan;
 
             var result = new List<string[]>();
             for (int i = 0; i < auctionRepresent.Count; i++)
@@ -63,15 +67,17 @@ namespace Coflnet.Sky.Api.Services
                 var desc = auctionRepresent[i].desc;
                 var auction = auctionRepresent[i].auction;
                 var price = res[i];
+                var craftPrice = allCrafts?.Where(c => auction != null && c.ItemId == auction.Tag && c.CraftCost > 0)?.FirstOrDefault()?.CraftCost;
+                if (desc != null)
+                    span.Log(JsonConvert.SerializeObject(auction, Formatting.Indented) + string.Join('\n', desc) + JsonConvert.SerializeObject(price, Formatting.Indented) + "\ncraft:" + craftPrice);
                 if (desc == null || price == null)
                     result.Add(null);
                 else if (desc.LastOrDefault()?.EndsWith("Click to open!") ?? false)
                     result.Add(desc.Append("this is the menu").ToArray());
-                else if (price.Volume == 0)
+                else if (price.Volume == 0 && !craftPrice.HasValue)
                     result.Add(desc.Append("no auction price data").ToArray());
                 else
                 {
-                    var craftPrice = allCrafts?.Where(c => auction != null && c.ItemId == auction.Tag && c.CraftCost > 0)?.FirstOrDefault()?.CraftCost;
                     var newOne = desc.AsEnumerable();
                     if (price.Lbin.Price > 0)
                         newOne = desc.Append($"lbin: {FormatNumber(price.Lbin.Price)}");
