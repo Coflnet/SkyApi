28b6a2a7d9eba97127a16595863017026e20b813
Äkwav <ekwav@coflnet.com>
2022-09-05 00:00:15 +0200

    document pruchaseargs

--- Models/PurchaseArgs.cs
+++ Models/PurchaseArgs.cs
@@ -1,9 +1,21 @@
 namespace Coflnet.Sky.Api.Models
 {
+    /// <summary>
+    /// Arguments for purchasing a service
+    /// </summary>
     public class PurchaseArgs
     {
+        /// <summary>
+        /// The service to pruchase
+        /// </summary>
         public string slug;
+        /// <summary>
+        /// How many instances to purchase (longer time)
+        /// </summary>
         public int count;
+        /// <summary>
+        /// Reference to prevent dupplicates
+        /// </summary>
         public string reference;
     }
 }
