48ac256775bb03630224b46c65d38eaec16072cd
Äkwav <ekwav@coflnet.com>
2022-09-22 22:07:05 +0200

    switch external api to ISO 8601 for mayor

--- Controllers/MayorController.cs
+++ Controllers/MayorController.cs
@@ -5,6 +5,8 @@ using Coflnet.Sky.Api.Models;
 using Coflnet.Sky.Api.Services;
 using Coflnet.Sky.Mayor.Client.Api;
 using Coflnet.Sky.Mayor.Client.Model;
+using System;
+using Coflnet.Sky.Core;
 
 namespace Coflnet.Hypixel.Controller;
 
@@ -39,17 +41,19 @@ public class MayorController : ControllerBase
     }
 
     /// <summary>
-    /// Gets election data between two unix timestamps (milliseconds)
+    /// Gets election data between two Timestamps
     /// </summary>
-    /// <param name="from">Start unix timestamp (milliseconds)</param>
-    /// <param name="to">End unix timestamp (milliseconds)</param>
+    /// <param name="from">Start ISO 8601</param>
+    /// <param name="to">End eg. 2022-09-22T20:03:10.937Z</param>
     /// <returns></returns>
     [Route("")]
     [HttpGet]
     [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any, NoStore = false, VaryByQueryKeys = new string[] { "from", "to" })]
-    public async Task<IEnumerable<ModelElectionPeriod>> GetMultiple(long from, long to)
+    public async Task<IEnumerable<ModelElectionPeriod>> GetMultiple(DateTime from, DateTime to = default(DateTime))
     {
-        return await mayorService.ElectionPeriodRangeGetAsync(from, to);
+        if(default(DateTime) == to)
+            to = DateTime.Now;
+        return await mayorService.ElectionPeriodRangeGetAsync(from.ToUnix() * 1000, to.ToUnix() * 1000);
     }
 }
 
