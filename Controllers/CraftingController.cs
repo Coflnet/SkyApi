a839495e00daff8a57575c9e028bc600e121a37a
Äkwav <ekwav@coflnet.com>
2022-12-17 19:54:55 +0100

    allow base_url in crafting controller

--- Controllers/CraftingController.cs
+++ Controllers/CraftingController.cs
@@ -32,9 +32,9 @@ namespace Coflnet.Hypixel.Controller
         public CraftingController(IConfiguration config, PricesService pricesService)
         {
             if (client == null)
-                client = new RestClient("http://" + config["CRAFTS_HOST"]);
+                client = new RestClient(config["CRAFTS_BASE_URL"] ?? "http://" + config["CRAFTS_HOST"]);
             if (profileClient == null)
-                profileClient = new RestClient("http://" + config["PROFILE_HOST"]);
+                profileClient = new RestClient(config["PROFILE_BASE_URL"] ?? "http://" + config["PROFILE_HOST"]);
             apiUrl = config["API_BASE_URL"];
             this.pricesService = pricesService;
         }
