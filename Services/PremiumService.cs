3afefd9170c4edfb4840034c9d86351879aa6069
Florian Mühlhans <muehlhans.florian@web.de>
2023-02-18 12:11:38 +0100

    add two counters to google signature validations

--- Services/PremiumService.cs
+++ Services/PremiumService.cs
@@ -1,3 +1,4 @@
+using Prometheus;
 using System;
 using Coflnet.Sky.Core;
 using Google.Apis.Auth;
@@ -6,11 +7,18 @@ namespace Coflnet.Sky.Api
 {
     public class PremiumService
     {
+
+        private Counter GoogleWebSignaturesValidatedSuccessfully = Metrics.CreateCounter("sky_api_google_web_signatures_validated_successfully", "The number of google web signatures validated successfully");
+
+        private Counter GoogleWebSignaturesValidationFailed = Metrics.CreateCounter("sky_api_google_web_signatures_validations_failed", "The number of exceptions while validating google web signatures");
+
+
         public GoogleUser GetUserWithToken(string token)
         {
 
             return UserService.Instance.GetOrCreateUser(ValidateToken(token).Subject);
         }
+
         public GoogleJsonWebSignature.Payload ValidateToken(string token)
         {
             try
@@ -18,10 +26,13 @@ namespace Coflnet.Sky.Api
                 var client = GoogleJsonWebSignature.ValidateAsync(token);
                 client.Wait();
                 var tokenData = client.Result;
+                
+                GoogleWebSignaturesValidatedSuccessfully.Inc();
                 return tokenData;
             }
             catch (Exception e)
             {
+                GoogleWebSignaturesValidationFailed.Inc();
                 throw new CoflnetException("invalid_token", $"{e.InnerException.Message}");
             }
         }
