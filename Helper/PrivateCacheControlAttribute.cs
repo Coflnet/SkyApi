ed27ba141e527e4934cc368665fae0ef9adb285c
Äkwav <ekwav@coflnet.com>
2023-08-02 22:38:39 +0200

    add docs to cache control

--- Helper/PrivateCacheControlAttribute.cs
+++ Helper/PrivateCacheControlAttribute.cs
@@ -4,19 +4,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Net.Http.Headers;
 
+/// <summary>
+/// Custom cache control
+/// </summary>
 public sealed class CacheControlAttribute : Attribute, IResultFilter
 {
+    /// <summary>
+    /// Cache duration in seconds
+    /// </summary>
     public int Duration { get; set; } = 0;
 
+    /// <summary>
+    /// Creates a new instance of <see cref="CacheControlAttribute"/>.
+    /// </summary>
+    /// <param name="duration"></param>
     public CacheControlAttribute(int duration)
     {
         Duration = duration;
     }
 
+    /// <summary>
+    /// Called after the action result executes.
+    /// </summary>
+    /// <param name="context"></param>
     public void OnResultExecuted(ResultExecutedContext context)
     {
     }
 
+    /// <summary>
+    /// Called before the action result executes.
+    /// </summary>
+    /// <param name="context"></param>
     public void OnResultExecuting(ResultExecutingContext context)
     {
         context.HttpContext.Response.OnStarting(state =>
