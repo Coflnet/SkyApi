c6237ce8834433a681f1d404573076dbbd9df3b5
Äkwav <ekwav@coflnet.com>
2023-11-08 19:33:11 +0100

    check pricing for null

--- Services/Description/ListPriceRecommend.cs
+++ Services/Description/ListPriceRecommend.cs
@@ -16,30 +16,21 @@ public class ListPriceRecommend : CustomModifier
 
     public static string GetRecommendText(PriceEstimate pricing, ModDescriptionService modService)
     {
-        if (pricing.Median <= 4_000_000)
+        if (pricing == null || pricing.Median <= 4_000_000)
         {
             return $"No recommended instasell from Coflnet";
         }
-        try
-        {
+        var deduct = 0.12;
+        if (pricing.Median < 15_000_000)
+            deduct = 0.18;
+        if (pricing.Median > 150_000_000)
+            deduct = 0.10;
+        var fromMed = pricing.Median * (1 - deduct);
+        var target = Math.Max(fromMed, Math.Min(pricing.Lbin.Price * (1 - deduct - 0.08), fromMed * 1.2));
+        if (pricing.ItemKey != pricing.LbinKey)
+            target = fromMed;
 
-            var deduct = 0.12;
-            if (pricing.Median < 15_000_000)
-                deduct = 0.18;
-            if (pricing.Median > 150_000_000)
-                deduct = 0.10;
-            var fromMed = pricing.Median * (1 - deduct);
-            var target = Math.Max(fromMed, Math.Min(pricing.Lbin.Price * (1 - deduct - 0.08), fromMed * 1.2));
-            if (pricing.ItemKey != pricing.LbinKey)
-                target = fromMed;
-
-            var formattedPrice = modService.FormatNumber(target);
-            return $"{McColorCodes.GREEN}Instasell: {McColorCodes.DARK_GREEN}{formattedPrice} {McColorCodes.WHITE}based on Coflnet data";
-        }
-        catch (Exception e)
-        {
-            modService.logger.LogError(e, "Error while calculating price {pricing}", JsonConvert.SerializeObject(pricing));
-            return $"No recommended instasell from Coflnet";
-        }
+        var formattedPrice = modService.FormatNumber(target);
+        return $"{McColorCodes.GREEN}Instasell: {McColorCodes.DARK_GREEN}{formattedPrice} {McColorCodes.WHITE}based on Coflnet data";
     }
 }
