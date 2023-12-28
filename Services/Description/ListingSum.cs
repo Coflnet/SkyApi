011359a4d31bd0e7ccb67e439b5893bc8eaefba3
Äkwav <ekwav@coflnet.com>
2023-12-28 14:32:43 +0100

    Add documentation

--- Services/Description/ListingSum.cs
+++ Services/Description/ListingSum.cs
@@ -1,10 +1,20 @@
 namespace Coflnet.Sky.Api.Services.Description;
 
+/// <summary>
+/// Container for auction metadata to generate listing cost and price paid from
+/// </summary>
 public class ListingSum
 {
+    /// <summary>
+    /// highest bid
+    /// </summary>
     public long highest { get; set; }
     public long StartingBid { get; set; }
     public DateTime end { get; set; }
+    /// <summary>
+    /// Whereever the mod user requesting the data is the same as the seller
+    /// Price paid doesn't want to have the highest bid of the user
+    /// </summary>
     public bool requestingUserIsSeller { get; set; }
     public DateTime start { get; set; }
 }
