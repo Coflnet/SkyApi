e95a55c7b2acb2337fbd7f664481fa899d554396
Äkwav <ekwav@coflnet.com>
2024-07-31 11:48:33 +0200

    document SkybockGemsValue adder

--- Services/Description/SkyblockGemsValue.cs
+++ Services/Description/SkyblockGemsValue.cs
@@ -4,11 +4,23 @@ using Coflnet.Sky.Sniper.Client.Model;
 
 namespace SkyApi.Services.Description;
 
+/// <summary>
+/// A <see cref="CurrencyValueDisplay"/> for SkyBlock Gems
+/// </summary>
 public class SkyblockGemsValue : CurrencyValueDisplay
 {
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     protected override string ValueSuffix => "SkyBlock Gems";
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     protected override string currencyName => "Gem";
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     protected override void ProcessLine(DataContainer data, int i, string[] desc, PriceEstimate price)
     {
         var auction = data.auctionRepresent[i].auction;
