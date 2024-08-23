196f225b176305743764457817bdb9d59dddc642
Äkwav <ekwav@coflnet.com>
2024-08-23 16:23:52 +0200

    log item name with uid when found

--- Services/Description/StartedAgoToEndsIn.cs
+++ Services/Description/StartedAgoToEndsIn.cs
@@ -17,18 +17,18 @@ public class StartedAgoToEndsIn : CustomModifier
             if (auction?.FlatenedNBT == null || !auction.FlatenedNBT.TryGetValue("uid", out var uid))
                 continue;
             var startTime = data.itemListings[uid].OrderByDescending(a => a.start).FirstOrDefault()?.start;
-            var timeSinceStart = DateTime.UtcNow - startTime;
 
             var line = desc.Select((l, i) => (l, i)).FirstOrDefault(l => l.l.StartsWith("§7Ends in"));
             if (line.i == 0)
             {
                 continue;
             }
-            if (timeSinceStart == null)
+            if (startTime == null)
             {
-                Console.WriteLine($"Found new auction " + string.Join("\n", desc));
+                Console.WriteLine($"Found new auction {auction.ItemName} - {uid}" + string.Join("\n", desc));
                 continue;
             }
+            var timeSinceStart = DateTime.UtcNow - startTime;
             var formatted = ModDescriptionService.FormatTime(timeSinceStart.Value);
             var position = desc.Length - 1;
             position += auction.Enchantments.Where(e => e.Type == Core.Enchantment.EnchantmentType.efficiency 
