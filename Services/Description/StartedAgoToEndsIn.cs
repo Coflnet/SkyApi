3937899c46c8fabb5b6193892f5fb2b6c913974d
Äkwav <ekwav@coflnet.com>
2024-08-31 22:26:56 +0200

    Handle auctions with no recent start fix #148

--- Services/Description/StartedAgoToEndsIn.cs
+++ Services/Description/StartedAgoToEndsIn.cs
@@ -8,7 +8,7 @@ public class StartedAgoToEndsIn : CustomModifier
 {
     public void Apply(DataContainer data)
     {
-        if(data.itemListings.Count == 0)
+        if (data.itemListings.Count == 0)
             return;
         for (int i = 0; i < data.auctionRepresent.Count; i++)
         {
@@ -18,29 +18,35 @@ public class StartedAgoToEndsIn : CustomModifier
             // get line index with .StartsWith("§7Ends in") 
             if (auction?.FlatenedNBT == null || !auction.FlatenedNBT.TryGetValue("uid", out var uid))
                 continue;
-            var startTime = data.itemListings[uid].OrderByDescending(a => a.start).FirstOrDefault()?.start;
+            var lastStart = data.itemListings[uid].OrderByDescending(a => a.start).FirstOrDefault();
+            var startTime = lastStart?.start;
+            var position = desc.Length - 1;
+            position += auction.Enchantments.Where(e => e.Type == Core.Enchantment.EnchantmentType.efficiency
+                                                || e.Type == Core.Enchantment.EnchantmentType.respiration
+                                                || e.Type == Core.Enchantment.EnchantmentType.aqua_affinity
+                                                || e.Type == Core.Enchantment.EnchantmentType.depth_strider).Count();
+            if (auction.FlatenedNBT.ContainsKey("color"))
+                position++;
+            if (desc.Last() == "§eClick to inspect!")
+                position -= 2;
 
             var line = desc.Select((l, i) => (l, i)).FirstOrDefault(l => l.l.StartsWith("§7Ends in"));
             if (line.i == 0)
             {
                 continue;
             }
-            if (startTime == null)
+            if (startTime == null || startTime < DateTime.UtcNow - TimeSpan.FromDays(14))
             {
                 Console.WriteLine($"Found new auction {auction.ItemName} - {uid}" + string.Join("\n", desc));
+                data.mods[i].Insert(0, new DescModification($"{McColorCodes.DARK_GRAY}Started {McColorCodes.GRAY}less than a minute {McColorCodes.DARK_GRAY}ago")
+                {
+                    Line = position,
+                    Type = DescModification.ModType.REPLACE
+                });
                 continue;
             }
             var timeSinceStart = DateTime.UtcNow - startTime;
             var formatted = ModDescriptionService.FormatTime(timeSinceStart.Value);
-            var position = desc.Length - 1;
-            position += auction.Enchantments.Where(e => e.Type == Core.Enchantment.EnchantmentType.efficiency 
-                                                || e.Type == Core.Enchantment.EnchantmentType.respiration 
-                                                || e.Type == Core.Enchantment.EnchantmentType.aqua_affinity
-                                                || e.Type == Core.Enchantment.EnchantmentType.depth_strider).Count();
-            if (auction.FlatenedNBT.ContainsKey("color"))
-                position++;
-            if(desc.Last() == "§eClick to inspect!")
-                position-=2;
             data.mods[i].Insert(0, new DescModification($"{McColorCodes.DARK_GRAY}Started {McColorCodes.GRAY}{formatted} {McColorCodes.DARK_GRAY}ago")
             {
                 Line = position,
