01c5740e04e8a133988f8f165a1cb83d51ac23e7
Äkwav <ekwav@coflnet.com>
2024-12-15 11:21:16 +0100

    dynamic order limit

--- Services/Description/BazaarOrderAdjust.cs
+++ Services/Description/BazaarOrderAdjust.cs
@@ -20,8 +20,9 @@ public class BazaarOrderAdjust : CustomModifier
     {
         var loaded = data.Loaded[nameof(BazaarOrderAdjust)].Result;
         var result = JsonConvert.DeserializeObject<StorageQuickStatus[]>(loaded).ToDictionary(x => x.ProductId);
-        var offerLookup = data.auctionRepresent.Take(36).Where(x => x.auction != null).ToLookup(x => (x.auction.ItemName.Contains("BUY"),x.auction.Tag));
-        for (int i = 0; i < 36; i++)
+        var slotCount = data.auctionRepresent.Count - 9 * 5;
+        var offerLookup = data.auctionRepresent.Take(slotCount).Where(x => x.auction != null).ToLookup(x => (x.auction.ItemName.Contains("BUY"), x.auction.Tag));
+        for (int i = 0; i < slotCount; i++)
         {
             var auction = data.auctionRepresent[i].auction;
             var description = data.auctionRepresent[i].desc;
@@ -31,13 +32,13 @@ public class BazaarOrderAdjust : CustomModifier
             if (bazaar != null && bazaar.SellPrice != 0)
             {
                 var isBuy = auction.ItemName.Contains("BUY");
-                var allPrices = offerLookup[(isBuy,auction.Tag)].Select(x => x.desc).Where(x => x != null)
+                var allPrices = offerLookup[(isBuy, auction.Tag)].Select(x => x.desc).Where(x => x != null)
                     .Select(x => x.Where(p => p.StartsWith("§7Price per unit: §6")).First().Split("§7Price per unit: §6").Last().Split(" coins").First())
                     .Select(v => double.Parse(v, System.Globalization.CultureInfo.InvariantCulture)).ToArray();
 
                 var price = isBuy ? allPrices.Max() : allPrices.Min();
                 var isTop = isBuy ? bazaar.SellPrice <= price : bazaar.BuyPrice >= price;
-                var isOnlyOne = offerLookup[(isBuy,auction.Tag)].Count() == 1;
+                var isOnlyOne = offerLookup[(isBuy, auction.Tag)].Count() == 1;
                 if (isTop)
                 {
                     var color = isOnlyOne ? "50af50" : "508f50";
@@ -53,7 +54,7 @@ public class BazaarOrderAdjust : CustomModifier
                     var ahead = list.Sum(o => o.Amount);
                     data.mods[i].Add(new(Models.Mod.DescModification.ModType.INSERT, 2, $"{McColorCodes.RED}{ModDescriptionService.FormatPriceShort(ahead)}{McColorCodes.GRAY} available for better price "));
                 }
-                if(description.Any(l=>l.EndsWith("100%!")))
+                if (description.Any(l => l.EndsWith("100%!")))
                 {
                     data.mods[i].Where(x => x.Type == Models.Mod.DescModification.ModType.HIGHLIGHT).First().Value = "00ff00";
                 }
@@ -70,7 +71,8 @@ public class BazaarOrderAdjust : CustomModifier
     {
         var task = Task.Run(async () =>
         {
-            var ids = preRequest.auctionRepresent.Take(36).Where(x => x.auction != null)
+            var slotCount = preRequest.auctionRepresent.Count - 9 * 5;
+            var ids = preRequest.auctionRepresent.Take(slotCount).Where(x => x.auction != null)
                     .Select(x => x.auction.Tag).Distinct().ToArray();
             var requests = ids.Select(x => bazaarApi.ApiBazaarItemIdSnapshotGetAsync(x));
             var result = await Task.WhenAll(requests);
