1101e41ceac9e11826f683d42acd6e8118c2264e
Äkwav <ekwav@coflnet.com>
2025-06-05 12:17:02 +0200

    highlight lbin or non lbin fix #174

--- Services/Description/AuctionValueSummary.cs
+++ Services/Description/AuctionValueSummary.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using Coflnet.Sky.Api.Models.Mod;
+using Coflnet.Sky.Commands.MC;
 
 namespace Coflnet.Sky.Api.Services.Description;
 
@@ -9,8 +10,10 @@ public class AuctionValueSummary : CustomModifier
     public void Apply(DataContainer data)
     {
         var sum = 0L;
-        foreach (var item in data.Items.Take(27))
+        for (int i = 0; i < 9 * 3; i++)
         {
+            var item = data.Items.ElementAtOrDefault(i);
+            var price = data.PriceEst.ElementAtOrDefault(i);
             if (item?.Description == null)
                 continue;
             var regexParsedPrice = Regex.Match(item.Description, @"Buy it now: §\d([\d,]+) coins");
@@ -21,6 +24,19 @@ public class AuctionValueSummary : CustomModifier
                 sum += value * 99 / 100;
             else
                 sum += value;
+
+            // highlight item based on lbin or not
+            var lbin = price.Lbin.Price;
+            if (lbin >= value || lbin == 0)
+            {
+                data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "80ff80")); // green for lbin
+                data.mods[i].Add(new DescModification(DescModification.ModType.INSERT, 1, $"{McColorCodes.DARK_GREEN}Is Lbin" + (lbin == 0 ? " (only offer)" : $" ({data.modService.FormatNumber(lbin-value)} lower than slbin)")));
+            }
+            else if (lbin < value)
+            {
+                data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "ff8080")); // red for not lbin
+                data.mods[i].Add(new DescModification(DescModification.ModType.INSERT, 1, $"{McColorCodes.RED}Not lbin ({data.modService.FormatNumber(value-lbin)} higher than lbin)"));
+            }
         }
         data.mods.Last().Insert(0, new DescModification(DescModification.ModType.REPLACE, 0, $"Auctions value: §6{data.modService.FormatNumber(sum)}"));
     }
