05280e74e76413939a361c2c8e65eb7ec92a18a7
Äkwav <ekwav@coflnet.com>
2025-06-08 00:28:39 +0200

    Revert "move price explanation to end of lore"
    This reverts commit 064163b344707649724383e5adbdf44ba9aad04f.

--- Services/Description/AuctionValueSummary.cs
+++ Services/Description/AuctionValueSummary.cs
@@ -34,12 +34,12 @@ public class AuctionValueSummary : CustomModifier
             if (lbin >= value || lbin == 0)
             {
                 data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "80ff80")); // green for lbin
-                data.mods[i].Add(new DescModification($"{McColorCodes.DARK_GREEN}Is Lbin" + (price.SLbin.Price == 0 ? " (only offer)" : $" ({data.modService.FormatNumber(price.SLbin.Price-value)} lower than slbin)")));
+                data.mods[i].Add(new DescModification(DescModification.ModType.INSERT, 1, $"{McColorCodes.DARK_GREEN}Is Lbin" + (price.SLbin.Price == 0 ? " (only offer)" : $" ({data.modService.FormatNumber(price.SLbin.Price-value)} lower than slbin)")));
             }
             else if (lbin < value)
             {
                 data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "ff0000")); // red for not lbin
-                data.mods[i].Add(new DescModification($"{McColorCodes.RED}Not lbin ({data.modService.FormatNumber(value-lbin)} higher than lbin)"));
+                data.mods[i].Add(new DescModification(DescModification.ModType.INSERT, 1, $"{McColorCodes.RED}Not lbin ({data.modService.FormatNumber(value-lbin)} higher than lbin)"));
             }
         }
         data.mods.Last().Insert(0, new DescModification(DescModification.ModType.REPLACE, 0, $"Auctions value: §6{data.modService.FormatNumber(sum)}"));
