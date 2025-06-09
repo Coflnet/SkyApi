f9209b491bb3e9eec1cc952e1d637935f12be55a
Äkwav <ekwav@coflnet.com>
2025-06-09 12:32:29 +0200

    adjust auction colors to bazaar

--- Services/Description/AuctionValueSummary.cs
+++ Services/Description/AuctionValueSummary.cs
@@ -25,7 +25,7 @@ public class AuctionValueSummary : CustomModifier
             {
                 var isSold = Regex.IsMatch(item.Description, @"Sold for: §\d([\d,]+) coins");
                 if (isSold)
-                    data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 1, "008f00"));
+                    data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 1, "00ff00"));
                 continue;
             }
             var value = long.Parse(regexParsedPrice.Groups[1].Value, System.Globalization.NumberStyles.AllowThousands, System.Globalization.CultureInfo.InvariantCulture);
@@ -38,7 +38,7 @@ public class AuctionValueSummary : CustomModifier
             var lbin = price.Lbin.Price;
             if (lbin >= value || lbin == 0)
             {
-                data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "80ff80")); // green for lbin
+                data.mods[i].Add(new DescModification(DescModification.ModType.HIGHLIGHT, 0, "50af50")); // green for lbin
                 data.mods[i].Add(new DescModification(DescModification.ModType.INSERT, 1, $"{McColorCodes.DARK_GREEN}Is Lbin" + (price.SLbin.Price == 0 ? " (only offer)" : $" ({data.modService.FormatNumber(price.SLbin.Price - value)} lower than slbin)")));
             }
             else if (lbin < value)
